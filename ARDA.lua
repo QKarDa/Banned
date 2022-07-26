31be01aed9259b18f6c60549db89da3adde3af18
QKarDa <110054019+QKarDa@users.noreply.github.com>
2022-07-26 20:50:04 +0300

    Update ARDA.lua

--- ARDA.lua
+++ ARDA.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 537413528 then
    local PlaceId = game.PlaceId
 
-local BooHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/QKarDa/Banned/main/ARDA.lua"))()
+local BooHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/zachisfunny/BooHub/main/Boo"))()
 
 local UI = BooHub.Load({
      Title = "Hacker Lab",
